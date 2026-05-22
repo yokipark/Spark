8eb38120b24013340eef91908ba295557b982e75
yokipark <yokiepark@gmail.com>
2026-05-22 15:39:55 +0600

    Update login.py

--- Spark/pages/login.py
+++ Spark/pages/login.py
@@ -2,11 +2,9 @@ import customtkinter as ctk
 import localization
 class LoginPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
-        # FIXED: Changed "transparent" to the exact green color from your design
         super().__init__(parent, fg_color="#4A6550")
         self.controller = controller
 
-        # Main card
         self.main_card = ctk.CTkFrame(
             self, 
             width=516, 
@@ -16,11 +14,10 @@ class LoginPage(ctk.CTkFrame):
         )
         self.main_card.place(relx=0.5, rely=0.5, anchor="center")
 
-        # Title
         self.title_label = ctk.CTkLabel(
             self.main_card,
             text="Картотека", 
-            font=("Helvetica", 28, "bold"), 
+            font=("Inter", 24), 
             text_color="black"
         )
         self.title_label.place(x=258, y=190, anchor="center")
@@ -28,12 +25,12 @@ class LoginPage(ctk.CTkFrame):
         self.subtitle = ctk.CTkLabel(
             self.main_card, 
             text="Библиотекаря", 
-            font=("Helvetica", 16), 
+            font=("Inter", 24), 
             text_color="black"
         )
         self.subtitle.place(x=258, y=220, anchor="center")
 
-        # --- NEW: Error Label (Hidden by default) ---
+        #Error Label
         self.error_label = ctk.CTkLabel(
             self.main_card, 
             text="", 
@@ -46,7 +43,7 @@ class LoginPage(ctk.CTkFrame):
         self.idsotrudnika = ctk.CTkLabel(
             self.main_card, 
             text=localization.get("ID сотрудника"), 
-            font=("Helvetica", 12), 
+            font=("Inter", 20), 
             text_color="black"
         )
         self.idsotrudnika.place(x=68, y=300, anchor="w")
@@ -62,7 +59,7 @@ class LoginPage(ctk.CTkFrame):
         self.pass_label = ctk.CTkLabel(
             self.main_card, 
             text=localization.get("Пароль"), 
-            font=("Helvetica", 12), 
+            font=("Inter", 20), 
             text_color="black"
         )
         self.pass_label.place(x=68, y=390, anchor="w")
